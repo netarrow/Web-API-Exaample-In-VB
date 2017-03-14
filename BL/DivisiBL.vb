71faf49274074dbdbbfc4999ae81f0e7c7e955fd
erick <erick@DESKTOP-7DP9Q9N>
2017-03-14 14:48:55 +0700

    Menambahkan BL Divisi

---
+++ BL/DivisiBL.vb
@@ -0,0 +1,9 @@
+﻿Imports BO
+Imports DAL
+
+Public Class DivisiBL
+    Public Function GetAllDivisi() As IEnumerable(Of Divisi)
+        Dim divisiDal As New DivisiDAL()
+        Return divisiDal.GetAllDivisi()
+    End Function
+End Class
