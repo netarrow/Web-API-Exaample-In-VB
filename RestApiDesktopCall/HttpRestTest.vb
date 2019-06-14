69e6940ba0f5cc7f2aac12175bed8c0285864768
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2019-06-14 09:28:07 +0200

    predisposto esempio chiamata da vb net per web api

---
+++ RestApiDesktopCall/HttpRestTest.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Net.Http
+
+Module HttpRestTest
+
+    Sub Main()
+        Dim t As Task = New Task(AddressOf SendRequest)
+        t.Start()
+        Console.WriteLine("Inizio richiesta")
+        Console.ReadLine()
+    End Sub
+
+    Async Sub SendRequest()
+        Dim uri As New Uri("http://localhost:25275/api/item")
+        Using client As HttpClient = New HttpClient
+            Using response As HttpResponseMessage = Await client.GetAsync(uri)
+                Using content As HttpContent = response.Content
+                    Dim result As String = Await content.ReadAsStringAsync()
+                    If result IsNot Nothing Then
+                        Console.WriteLine(result.ToString())
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+
+End Module
---
+++ RestApiDesktopCall/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RestApiDesktopCall")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("RestApiDesktopCall")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bfd83909-6f5b-4528-8392-5e0fb7c71dd9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
